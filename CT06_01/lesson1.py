2bca5d34d7fb7a3471ca9d72646fe0d0735c3706
zirui20 <firefishpro@gmail.com>
2025-01-12 03:42:32 +0000

    Jan 12, 2025, 3:42 AM

--- CT06_01/lesson1.py
+++ CT06_01/lesson1.py
@@ -1,12 +1,7 @@
 print("Hello from lesson 1")
 
 ######### The below is the code required for task 2a ##########
-# print("1")
-# print("2")
-# print("3")
-# print("4")
-# print("5")
-# print("6")
+#
 # print("7")
 # print("8")
 # print("9")
