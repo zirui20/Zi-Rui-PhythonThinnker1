4b0861b52ca4a87cdd9dddb25353a19722d928f5
zirui20 <firefishpro@gmail.com>
2025-01-12 11:25:30 +0800

    Initial commit

---
+++ CT06_02/lesson2.py
@@ -0,0 +1,7 @@
+print("Hello from lesson 2")
+
+######## Write the pseudocode in comments for task 2 here
+# Using comments, translate the code shown on screen into pseudocode.
+
+######## Write the pseudocode in comments for task 3 here
+# Using comments, translate the code shown on screen into pseudocode.
