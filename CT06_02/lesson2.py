f8fcb98a5e50c6561a32447ff7eb1d6dd54dd940
zirui20 <firefishpro@gmail.com>
2025-01-17 11:01:20 +0000

    Jan 17, 2025, 11:01 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -18,27 +18,27 @@ print("Hello from lesson 2")
 # ans= num1 + num2
 # print(ans)
 
-#  get student's test1 score
-test1 = input("what is your test1 score? ")
-test1 = int(test1) #converion
-#  get student's test2 score
-test2 = input("what is your test2 score? ")
-test2 = int(test2) #converion
-#  get student's test3 score
-test3 = input("what is your test3 score? ")
-test3 = int(test3) #converion
+# #  get student's test1 score
+# test1 = input("what is your test1 score? ")
+# test1 = int(test1) #converion
+# #  get student's test2 score
+# test2 = input("what is your test2 score? ")
+# test2 = int(test2) #converion
+# #  get student's test3 score
+# test3 = input("what is your test3 score? ")
+# test3 = int(test3) #converion
 
 
-# get studemt's test 2 score
-# get student's test 3 score
+# # get studemt's test 2 score
+# # get student's test 3 score
 
-# 0.2 multiply by the given test1 score
-# 0.4 multiply by the given test2 score
-# 0.4 multiply by the given test3 score
-# adds up these 3 numbers into a new variable named "final score"
-# print the final score
-final_score = 0.2 * test1 + 0.4 * test2 +0.4 * test3
-print("Your final score is", final_score)
+# # 0.2 multiply by the given test1 score
+# # 0.4 multiply by the given test2 score
+# # 0.4 multiply by the given test3 score
+# # adds up these 3 numbers into a new variable named "final score"
+# # print the final score
+# final_score = 0.2 * test1 + 0.4 * test2 +0.4 * test3
+# print("Your final score is", final_score)
 
 
 # save the input value into a variable named "num1"
