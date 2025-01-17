c45c4b8cb31fa43d8da0063008cfcd5d02f3a9b6
zirui20 <firefishpro@gmail.com>
2025-01-17 10:27:42 +0000

    Jan 17, 2025, 10:27 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -5,28 +5,7 @@ print("Hello from lesson 2")
 
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
-print("red")
-print("Orange")
-print("Yellow")
-print("Green")
-print("Blue")
-print("indigo")
-print("Violet")
-# repeat 10 times:
-#  say "Hey!"
-#  move 10 steps
-# assign 0 to counter
-# while "counter" is not 50:
-# move 10 steps
-# turn 15 degrees
-# increase "counter" by 10
-#(say how old are you )
-# (wait 5 seconds) 
-# (if answer is under 18 print ("Access Denied")
-# look at the material of the item
-# if item's material is plastic, put into the plastic bin
-# if item"s material is glass, put into glass bin
-# if items material is paper, put into paper bin
+
 # Check if secret code is correct
 # If it is, hello
 # say"Granted"
