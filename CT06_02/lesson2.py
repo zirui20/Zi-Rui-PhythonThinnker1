1f07fbcd8917dd0848e93eec878df18190f93f44
zirui20 <firefishpro@gmail.com>
2025-01-17 10:19:21 +0000

    Jan 17, 2025, 10:19 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -23,7 +23,6 @@ print("Violet")
 #(say how old are you )
 # (wait 5 seconds) 
 # (if answer is under 18 print ("Access Denied")
-# (if answer is above 18 print ("Access Granted"
 # (look at the material of the item
 # (if item's material is plastic, put into the plastic bin)
 # (if item"s material is glass, put into glass bin)
