8387cf68f6deebc39d2736f4e08cfe7bd8753bad
zirui20 <firefishpro@gmail.com>
2025-01-17 10:48:34 +0000

    Jan 17, 2025, 10:48 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -9,18 +9,6 @@ print("Hello from lesson 2")
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
 
-# Check if secret code is correct
-# If it is, hello
-# say"Granted"
-# Otherwise
-# say"Denied"
-
-# ask for test score 
-# Student say "20 percent, 40 percent and 40 percent" # type: ignore
-# Calculate all the test score together
-# say "Final score is 100 percent"
-
-
 #  get student's test1 score
 test1 = input("what is your test1 score?")
 test1 = int(test1) #converion
