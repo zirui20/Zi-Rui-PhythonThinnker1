b799a75de55955dde2bfa7e06b1d2f3bab99b857
zirui20 <firefishpro@gmail.com>
2025-01-17 10:54:21 +0000

    Jan 17, 2025, 10:54 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -45,7 +45,6 @@ print("Your final score is", final_score)
 # save another input value into a variable named "num2"
 # add the 2 numbers together
 # print the final result
-num1 = int()
-num2 = input("Give me the next number? ")
+num2 = input(("Give me the next number? "))
 ans= num1 + num2
 print(ans)
