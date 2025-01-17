c24cc6f0fb2e6a1e82d4db9b3d93eb825924d3da
zirui20 <firefishpro@gmail.com>
2025-01-17 09:43:49 +0000

    Jan 17, 2025, 9:43 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -5,4 +5,5 @@ print("Hello from lesson 2")
 
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
-print("")
+print("red")
+print("Oran")
