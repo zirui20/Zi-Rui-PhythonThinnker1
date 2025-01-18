41ac6708093646684122fa17933ff0042cbaa739
zirui20 <firefishpro@gmail.com>
2025-01-18 09:18:23 +0000

    Jan 18, 2025, 9:18 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -47,7 +47,7 @@ print("Hello from lesson 2")
 # print the final result
 num1 = input ("How many dogs did you eat today? ") 
 num1 = int(num1)
-num2 = input("Give me the next number? ") # conversion
+num2 = input("How many did you eat yesterday ") # conversion
 num2 = int(num2)
 ans= num1 + num2 # conversion
 print(ans)
