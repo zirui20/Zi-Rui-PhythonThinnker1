0dbe68977341023ceba86e778ad36c3d5f44167d
zirui20 <firefishpro@gmail.com>
2025-01-18 09:32:02 +0000

    Jan 18, 2025, 9:32 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -47,7 +47,7 @@ print("Hello from lesson 2")
 # print the final result
 num1 = input ("What did you get on your test? ") 
 num1 = int(num1)
-num2 = input("On your  ") # conversion
+num2 = input("On your WA2? ") # conversion
 num2 = int(num2)
 ans= num1 + num2 # conversion
 print(ans)
