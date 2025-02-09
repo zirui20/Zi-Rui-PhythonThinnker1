9ef9399366d172892377db62d10585c9291f6e29
zirui20 <firefishpro@gmail.com>
2025-02-09 08:29:02 +0000

    Feb 9, 2025, 8:29 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,4 +1,4 @@
 # print("Hello from lesson)
 Name = input ("What is your name")
-Age = input ("what age are you turning this year")
-Message = input ("What would you like to tell them")
+Age = input ("what age is  you turning this year")
+Message = input ("What would you like to tell them?")
