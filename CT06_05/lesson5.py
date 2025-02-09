ad8f3029c1119d07022b62f49a47d6f2a3686a45
zirui20 <firefishpro@gmail.com>
2025-02-09 08:52:12 +0000

    Feb 9, 2025, 8:52 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -6,5 +6,9 @@
 # print("Happy " + Age+"th birthday,"+ Name,+ Message)
 
 
+# for i in range(100):
+#     print("I like Chicken Rice" )
+
 for i in range(100):
-    print("I like Chicken Rice" )
+    print()
+
