b1b4ac5cc102f467a9ae432c45abe7e122cef2bb
zirui20 <firefishpro@gmail.com>
2025-02-09 08:49:55 +0000

    Feb 9, 2025, 8:49 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,6 +1,10 @@
-# print("Hello from lesson)
-Name = input ("What your friend's name? ")
-Age = input ("what age is he turning this year? ")
-Message = input ("What would you like to tell them? ")
+# # print("Hello from lesson)
+# Name = input ("What your friend's name? ")
+# Age = input ("what age is he turning this year? ")
+# Message = input ("What would you like to tell them? ")
 
-print("Happy " + Age+"th birthday,"+ Name,+ Message)
+# print("Happy " + Age+"th birthday,"+ Name,+ Message)
+
+
+for i in range(100):
+    print()
