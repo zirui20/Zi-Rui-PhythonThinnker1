f96313327f34ada2868f3b7016a70f8d6a5debed
zirui20 <firefishpro@gmail.com>
2025-02-16 08:50:49 +0000

    Feb 16, 2025, 8:50 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,10 +1,13 @@
 # print("Hello from lesson 6")
 
-Students = int(input("How many students are there in the class? "))
-TotScore = 0
+# Students = int(input("How many students are there in the class? "))
+# TotScore = 0
 
-for i in range(Students):
-    Score = int(input("What did this student get? "))
-    TotScore = TotScore + Score
+# for i in range(Students):
+#     Score = int(input("What did this student get? "))
+#     TotScore = TotScore + Score
 
-print("The average score is: " + str(TotScore/Score))
+# print("The average score is: " + str(TotScore/Score))
+
+for i in range(3):
+    print("Hello, World!")
