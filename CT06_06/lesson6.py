a58db47001277a845925f649c65a4bddb9ac59bb
zirui20 <firefishpro@gmail.com>
2025-02-16 08:36:00 +0000

    Feb 16, 2025, 8:36 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -2,6 +2,7 @@ print("Hello from lesson 6")
 
 Students = int(input("How many students are there in the class? "))
 TotScore = 0
+
 for i in range(Students):
     Score = int(input("What did this student get? "))
     TotScore = TotScore + Score
