d1ca8bcdf954fa9517aa94f01fc481f4f674324a
zirui20 <firefishpro@gmail.com>
2025-02-16 08:32:06 +0000

    Feb 16, 2025, 8:32 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,5 +1,4 @@
 print("Hello from lesson 6")
 
-Students= int(input("How many students are there in the class?"))
-for i in range(Students):
-    print("What is your mark?")
+Students = int(input("How many students are there in the class?"))
+Score = 0
