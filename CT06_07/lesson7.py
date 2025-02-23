50f296b691736973961446571fd4235d223da763
zirui20 <firefishpro@gmail.com>
2025-02-23 13:41:10 +0000

    Feb 23, 2025, 1:41 PM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,2 +1,42 @@
-print("Hello from lesson 7")
+# print("Hello from lesson 7")
 
+# score_one = 80
+# score_two = 90
+# score_three = 75
+
+# total = score_one + score_two + score_three
+
+# average_score = total / 3
+
+# student_name = "Alex"
+
+# print("Average score for " + student_name + " is: " + str(average_score))
+
+
+# for i in range(10):
+#     print(i+1)
+
+# for i in range(2,21,2):
+#     print(i)
+
+# for i in range(10,0,-1):
+#     print(i)
+
+# word = input("What word would you like to repeat? ")
+# repeat = int(input("How many times would you want to repeat it? "))
+
+# for i in range(repeat):
+#     print(word)
+
+# sum = 0
+
+# for i in range(1, 6):
+#     num = int(input("What is number #" + str(i) + "?"))
+#     sum = sum+num 
+
+# print("The sum of the numbers is: " + str(sum))
+
+num = int(input("What number do you want to know? "))
+
+for i in range(1, 13):
+    print(str(num) + " x " + str(i) + " = " + str(num * i))
