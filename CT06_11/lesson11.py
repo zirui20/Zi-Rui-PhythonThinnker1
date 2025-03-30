49aa274c155c4a29749cc98866854adedafb1731
zirui20 <firefishpro@gmail.com>
2025-03-30 08:30:58 +0000

    Mar 30, 2025, 8:30 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -14,7 +14,7 @@
 
 num = input("Can you gimme a number? ")
 
-if num%3:
+if num%3 == 0:
     print("The number is divisible by 3 and 7")
 else:
     print("This number is not divisible by 3 and 7")
