55bd5e93c47501ff229ed1b0c6769ef7107391fc
zirui20 <firefishpro@gmail.com>
2025-03-30 08:07:02 +0000

    Mar 30, 2025, 8:07 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,1 +1,42 @@
-print("Hello from lesson 10")
+# print("Hello from lesson 10")
+
+# num = int(input("Give me a number "))
+
+# if num <= 0:
+#     print((str(num)) "is a positive number")
+
+# else:
+#     print(str(num) + " is a negative number")
+
+
+# Number = int(input("What number do you want? "))
+
+# if Number%2 == 0:
+#     print("This is an even number")
+
+# else:
+#     print("This is an odd number")
+
+# age = int(input("What is your age? "))
+
+# if age < 13:
+#     print("You are a child")    
+# else:
+#     if age < 19:
+#         print("You are a Teen")
+
+#     else:
+#         print("You are an adult")
+
+Score = int(input("What is your score? "))
+
+if Score < 60:
+    print("Failure")
+elif Score <= 69:
+    print("You got a D")
+elif Score <= 70:
+    print("You got a C")
+elif Score <= 89:
+    print("You got a B")
+elif Score <= 100:
+    print("You got an A!! :D")
