2c9c89e70fd312bd31e051c1f44a4f5789f2c393
zirui20 <firefishpro@gmail.com>
2025-05-18 09:23:25 +0000

    May 18, 2025, 9:23 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -161,10 +161,7 @@ dy = 2
 
 def check_x(ball, screenWidth):
     if ball.xcor() > (screenWidth/2) or ball.xcor() < (-screenWidth/2):
-        return True
-
-dx = 2
-dy = 2    
+        return True 
 
 while True:
     move_ball(ball, dx, dy)
