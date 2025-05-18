e5a141366c62f1e840aa68928420330ef7b455ed
zirui20 <firefishpro@gmail.com>
2025-05-18 09:27:31 +0000

    May 18, 2025, 9:27 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -171,11 +171,8 @@ while True:
     move_ball(ball, dx, dy)
     if check_x(ball, screenWidth):
         dx *= -1
-    if check_y(ball, screenWidth):
+    if check_y(ball, screenHeight):
         dy *= -1
-
-while True:
-    move_ball(ball, dx, dy)
    
 
 window.mainloop()
