1a6b290fccc7347aa43e37f3de5e737719ab59b8
zirui20 <firefishpro@gmail.com>
2025-05-18 08:40:37 +0000

    May 18, 2025, 8:40 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -137,5 +137,7 @@ import turtle
 def setup_screen(screenWidth,screenHeight):
     screen = turtle.Screen()
     screen.setup(width =screenWidth , height= screenHeight)
-    return
+    return screen
+
+screen
 
